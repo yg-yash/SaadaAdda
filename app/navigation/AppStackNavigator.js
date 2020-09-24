bc86370ee0bc24923ec1f32c8acb4166833a0e97
Yash Gupta <yg123yash@gmail.com>
2020-09-24 19:32:43 +0530

    Update AppStackNavigator.js

--- app/navigation/AppStackNavigator.js
+++ app/navigation/AppStackNavigator.js
@@ -1,16 +1,1 @@
-import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
-import MainTabNavigator from './MainTabNavigator';
-const Stack = createStackNavigator();
 
-const AppStackNavigator = () => (
-  <Stack.Navigator
-    initialRouteName="Dashboard"
-    screenOptions={{
-      headerShown: false,
-    }}>
-    <Stack.Screen name="Dashboard" component={MainTabNavigator} />
-  </Stack.Navigator>
-);
-
-export default AppStackNavigator;
